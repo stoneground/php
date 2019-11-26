cdffb9b73b5d8085e0f8423b3d7449e9604c66b0
stoneground <43848949+stoneground@users.noreply.github.com>
2019-11-26 19:17:03 +0100

    Add files via upload

---
+++ wsp1-php/test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+</head>
+<body>
+    <?php
+     $name = "Nameless";
+    echo "<h1>Hello  $name</h1>";
+
+ 
+
+    $tal1 = 12; //integer
+    $tal2 = (int)"235";
+    $tal3 = true;
+    $tal4 = (int)"235,235";
+    $tal5 = null;
+
+    echo $tal1 + $tal2 + $tal3 + $tal4 + $tal5;
+    ?>
+</body>
+<a href="https://www.youtube.com/watch?v=sY_Yf4zz-yo">The best Chef ever</a>
+</html>
